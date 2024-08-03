818bcf24d2c7dfe6c5995318dfdc9c597f41ddd9
Marko (ServerlessLife) <marko@serverlesslife.com>
2024-08-03 15:33:42 +0200

    chore: configure lint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginPrettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    ignores: [
+      "**/dist/**",
+      "**/cdk.out/**",
+      "**/.lldebugger/**",
+      "**/.aws-sam/**",
+      "src/extension/aws/*.js",
+    ],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...pluginPrettier.configs.recommended.rules,
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
+  prettierConfig,
+];
