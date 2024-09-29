ade0e02cf33ffc69f9811286495cca5058f833e3
Marko (ServerlessLife) <marko@serverlesslife.com>
2024-09-29 18:54:28 +0200

    fix: CDK error Cannot find entry file

--- test/cdk-basic/bin/cdk-basic.ts
+++ test/cdk-basic/bin/cdk-basic.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkbasicStack } from '../lib/cdk-basic-stack';
+import { CdkbasicStack2 } from '../lib/subfolder/cdk-basic-stack2';
 
 const app = new cdk.App();
 
@@ -9,17 +10,8 @@ const environment = 'test';
 
 new CdkbasicStack(app, 'CdkbasicStack', {
   stackName: `${environment}-lld-cdk-basic`,
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+});
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new CdkbasicStack2(app, 'CdkbasicStack2', {
+  stackName: `${environment}-lld-cdk-basic2`,
 });
