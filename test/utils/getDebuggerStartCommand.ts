aef03ddaf4a722cd558b6ee3b167165f7f3d6a1e
Marko (ServerlessLife) <marko@serverlesslife.com>
2025-03-05 13:59:29 +0100

    WIP

--- test/utils/getDebuggerStartCommand.ts
+++ test/utils/getDebuggerStartCommand.ts
@@ -17,13 +17,17 @@ export function getDebuggerStartCommand(folder: string, args: string[]) {
 
   args.push('-v');
 
-  let command = `node ${testMonorepo ? '' : '../../'}dist/lldebugger.mjs ${args?.join(' ')}`;
+  let command: string;
 
   if (process.env.REAL_NPM === 'true') {
     console.log('Running the debugger with the real NPM');
     command = `lld ${args?.join(' ')}`;
   } else {
+    command = `node ${testMonorepo ? '' : '../../'}dist/lldebugger.mjs ${args?.join(' ')}`;
     console.log('Running the debugger with just genereted code');
   }
+  console.log(`Command to run LLD: ${command}`);
+  console.log(`Current folder: ${import.meta.dirname}`);
+
   return command;
 }
