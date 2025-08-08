43938541d930bd7d2bcfe8740bde99a3c5e80003
Marko <marko@serverlesslife.com>
2025-08-08 20:32:24 +0200

    fix: Update NPM dependencies (#131)
    * fix: Update dependencies
    
    * fix: CICD

--- test/cdk-basic/deploy-yaml.sh
+++ test/cdk-basic/deploy-yaml.sh
@@ -1,5 +1,6 @@
 # This script is used to deploy the CDK stack using a YAML template.
 npx cdk synth -c environment=test CdkbasicStack > CdkbasicStack.yaml
+
 # Add a dummy resource to the template to force a change in the stack
 awk '
   /^Resources:/ && !injected {
@@ -11,5 +12,10 @@ awk '
     next
   }
   { print }
-' CdkbasicStack.yaml > template.patched.yaml && mv template.patched.yaml CdkbasicStack.yaml
+' CdkbasicStack.yaml > template.patched.yaml
+
+# Cdk synth sometimes generates a dummy notification line at the top of the file.
+# Remove the first part of the template up to and including the Resources section
+awk 'f{print} /^Resources:/ {f=1; print}' template.patched.yaml > CdkbasicStack.yaml
+
 aws cloudformation deploy --template-file CdkbasicStack.yaml --stack-name test-lld-cdk-basic --capabilities CAPABILITY_NAMED_IAM
