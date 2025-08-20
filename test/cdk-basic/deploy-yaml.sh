2746309ff2f88ed4ac97e6aa9fde9965afeadc03
Marko (ServerlessLife) <marko@serverlesslife.com>
2025-08-20 08:52:50 +0200

    chore: add CICD logs

--- test/cdk-basic/deploy-yaml.sh
+++ test/cdk-basic/deploy-yaml.sh
@@ -18,4 +18,9 @@ awk '
 # Remove the first part of the template up to and including the Resources section
 awk 'f{print} /^Resources:/ {f=1; print}' template.patched.yaml > CdkbasicStack.yaml
 
+echo "Deploying stack with the following template:"
+echo "-------------------------------------------"
+cat CdkbasicStack.yaml
+echo "-------------------------------------------"
+
 aws cloudformation deploy --template-file CdkbasicStack.yaml --stack-name test-lld-cdk-basic --capabilities CAPABILITY_NAMED_IAM
